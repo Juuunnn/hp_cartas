be6020b7f9b08f59a78ac593dfb6ec483cb614ff
Juuunnn <jpablo.uni@gmail.com>
2022-11-25 02:16:57 -0700

    pruebas para obtener la informacion de un string JSON valido se cumple cuando es solo un objeto pero no cuando es un objeto dentro de un arreglo

--- test/domain/character_test.dart
+++ test/domain/character_test.dart
@@ -64,5 +64,91 @@ void main() {
               ),
           throwsA(isA<InvalidGender>()));
     });
+    test('poder ser generado de un json', () {
+      expect(
+          HPCharacter.fromJson(
+              '''{
+    "name": "Harry Potter",
+    "alternate_names": [],
+    "species": "human",
+    "gender": "male",
+    "house": "Gryffindor",
+    "dateOfBirth": "31-07-1980",
+    "yearOfBirth": 1980,
+    "wizard": true,
+    "ancestry": "half-blood",
+    "eyeColour": "green",
+    "hairColour": "black",
+    "wand": {
+      "wood": "holly",
+      "core": "phoenix feather",
+      "length": 11
+    },
+    "patronus": "stag",
+    "hogwartsStudent": true,
+    "hogwartsStaff": false,
+    "actor": "Daniel Radcliffe",
+    "alternate_actors": [],
+    "alive": true,
+    "image": "https://hp-api.herokuapp.com/images/harry.jpg"
+  }'''),
+          HPCharacter.constructor(
+            nameProp: 'Harry Potter',
+            speciesProp: 'human',
+            houseProp: 'Gryffindor',
+            genderProp: 'male',
+            dateOfBirthProp: '31-07-1980',
+            ancestryProp: 'half-blood',
+            patronusProp: 'stag',
+            wandProp:
+                Barita(wood: "holly", core: "phoenix feather", length: 11),
+            hogwartsStudentProp: true,
+            hogwartsStaffProp: false,
+            imageUrl: 'https://hp-api.herokuapp.com/images/harry.jpg',
+          ));
+    });
+    test('poder ser generado de un json nesteado dentro de un arreglo', () {
+      expect(
+          HPCharacter.fromJson(
+              '''[{
+    "name": "Harry Potter",
+    "alternate_names": [],
+    "species": "human",
+    "gender": "male",
+    "house": "Gryffindor",
+    "dateOfBirth": "31-07-1980",
+    "yearOfBirth": 1980,
+    "wizard": true,
+    "ancestry": "half-blood",
+    "eyeColour": "green",
+    "hairColour": "black",
+    "wand": {
+      "wood": "holly",
+      "core": "phoenix feather",
+      "length": 11
+    },
+    "patronus": "stag",
+    "hogwartsStudent": true,
+    "hogwartsStaff": false,
+    "actor": "Daniel Radcliffe",
+    "alternate_actors": [],
+    "alive": true,
+    "image": "https://hp-api.herokuapp.com/images/harry.jpg"
+  }]'''),
+          HPCharacter.constructor(
+            nameProp: 'Harry Potter',
+            speciesProp: 'human',
+            houseProp: 'Gryffindor',
+            genderProp: 'male',
+            dateOfBirthProp: '31-07-1980',
+            ancestryProp: 'half-blood',
+            patronusProp: 'stag',
+            wandProp:
+                Barita(wood: "holly", core: "phoenix feather", length: 11),
+            hogwartsStudentProp: true,
+            hogwartsStaffProp: false,
+            imageUrl: 'https://hp-api.herokuapp.com/images/harry.jpg',
+          ));
+    });
   });
 }
