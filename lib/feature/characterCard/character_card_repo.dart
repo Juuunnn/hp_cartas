ad6947f26653e4822b194f0765b10a32526b28b7
Juuunnn <jpablo.uni@gmail.com>
2022-11-25 07:34:21 -0700

    ahora puedo obtener un personaje de la lista de personajes

--- lib/feature/characterCard/character_card_repo.dart
+++ lib/feature/characterCard/character_card_repo.dart
@@ -1,15 +1,34 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:fpdart/fpdart.dart';
 import 'package:hp_cartas/domain/character.dart';
 import 'package:hp_cartas/domain/problem.dart';
 
 abstract class CharacterCardRepo {
-  Either<Problem, HPCharacter> getCharacterData();
+  Either<Problem, HPCharacter> getCharacterData(
+      {required String characterName});
 }
 
 class CharacterCardRepoTest extends CharacterCardRepo {
   @override
-  Either<Problem, HPCharacter> getCharacterData() {
-    // TODO: implement getCharacterData
-    throw UnimplementedError();
+  Either<Problem, HPCharacter> getCharacterData(
+      {required String characterName}) {
+    final elJson = File('test/characters.json').readAsStringSync();
+    return getDataFromList(elJson, characterName);
+  }
+}
+
+Either<Problem, HPCharacter> getDataFromList(
+    String ejJson, String characterName) {
+  try {
+    List<dynamic> listaCharacters = jsonDecode(ejJson);
+    final resultado = listaCharacters.firstWhere(
+      (element) => element["name"] == characterName,
+    );
+
+    return right(HPCharacter.fromMap(resultado));
+  } catch (e) {
+    return left(UnknownProblem(e.toString()));
   }
 }
--- test/feature/characterCard/character_card_repo_test.dart
+++ test/feature/characterCard/character_card_repo_test.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hp_cartas/domain/character.dart';
 import 'package:hp_cartas/feature/characterCard/character_card_repo.dart';
@@ -8,7 +6,7 @@ void main() {
   group('character card repo deve', () {
     test('poder obtener los datos', () {
       final repo = CharacterCardRepoTest();
-      final resultado = repo.getCharacterData();
+      final resultado = repo.getCharacterData(characterName: 'Harry Potter');
       resultado.match((l) {
         assert(false);
       }, (r) {
@@ -29,6 +27,58 @@ void main() {
               imageUrl: 'https://hp-api.herokuapp.com/images/harry.jpg',
             ));
       });
+    });
+    test(
+        'poder obtener un HPCharacter desde un json que es un arreglo de objetos',
+        () {
+      var resultado = getDataFromList('''[{
+    "name": "Harry Potter",
+    "alternate_names": [],
+    "species": "human",
+    "gender": "male",
+    "house": "Gryffindor",
+    "dateOfBirth": "31-07-1980",
+    "yearOfBirth": 1980,
+    "wizard": true,
+    "ancestry": "half-blood",
+    "eyeColour": "green",
+    "hairColour": "black",
+    "wand": {
+      "wood": "holly",
+      "core": "phoenix feather",
+      "length": 11
+    },
+    "patronus": "stag",
+    "hogwartsStudent": true,
+    "hogwartsStaff": false,
+    "actor": "Daniel Radcliffe",
+    "alternate_actors": [],
+    "alive": true,
+    "image": "https://hp-api.herokuapp.com/images/harry.jpg"
+  }]''', 'Harry Potter');
+      resultado.match(
+        (l) {
+          assert(false);
+        },
+        (r) {
+          expect(
+              r,
+              HPCharacter.constructor(
+                nameProp: 'Harry Potter',
+                speciesProp: 'human',
+                houseProp: 'Gryffindor',
+                genderProp: 'male',
+                dateOfBirthProp: '31-07-1980',
+                ancestryProp: 'half-blood',
+                patronusProp: 'stag',
+                wandProp:
+                    Barita(wood: "holly", core: "phoenix feather", length: 11),
+                hogwartsStudentProp: true,
+                hogwartsStaffProp: false,
+                imageUrl: 'https://hp-api.herokuapp.com/images/harry.jpg',
+              ));
+        },
+      );
     });
   });
 }
