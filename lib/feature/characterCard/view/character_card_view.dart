68699c52018b72d9c6ba957d3e4a95e51ef89577
Juuunnn <jpablo.uni@gmail.com>
2022-11-25 08:57:17 -0700

    CharacterCardView ahora tiene una tabla para mostrar su contenido

--- lib/feature/characterCard/view/character_card_view.dart
+++ lib/feature/characterCard/view/character_card_view.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:hp_cartas/domain/character.dart';
 
 class CharacterCardView extends StatelessWidget {
@@ -7,25 +10,57 @@ class CharacterCardView extends StatelessWidget {
   final HPCharacter character;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(character.name),
-        CachedNetworkImage(
-          imageUrl: character.image ??
-              'https://via.placeholder.com/728x90.jpeg?text=No+Image+data',
-          placeholder: (context, url) => const CircularProgressIndicator(),
-          errorWidget: (context, url, error) => const Icon(Icons.error),
-        ),
-        Text(character.ancestry),
-        Text(character.dateOfBirth),
-        Text(character.gender),
-        Text(character.house),
-        Text(character.patronus),
-        Text(character.species),
-        Text(character.hogwartsStaff.toString()),
-        Text(character.hogwartsStudent.toString()),
-        WandaInfo(wand: character.wand),
-      ],
+    Map<String, dynamic> characterMap = jsonDecode(character.toJson());
+    characterMap.removeWhere((key, value) => [
+          'name',
+          'wand',
+          'hogwartsStaff',
+          'hogwartsStudent',
+          'image'
+        ].contains(key));
+    return SizedBox(
+      width: 300,
+      child: Column(
+        children: [
+          Text(character.name),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CachedNetworkImage(
+              imageUrl: character.image ??
+                  'https://via.placeholder.com/728x90.jpeg?text=No+Image+data',
+              placeholder: (context, url) => const CircularProgressIndicator(),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+          ),
+          Table(
+            // defaultColumnWidth: const FlexColumnWidth(100),
+            border: TableBorder.symmetric(
+              outside: const BorderSide(color: Colors.black),
+              inside: const BorderSide(color: Colors.black),
+            ),
+            children: [
+              ...characterMap.entries.map((e) {
+                return TableRow(children: [
+                  Text(e.key),
+                  Center(child: Text(e.value.toString()))
+                ]);
+              }).toList(),
+              TableRow(children: [
+                const Text('hogwartsStaff'),
+                Center(child: Text(character.hogwartsStaff.toString()))
+              ]),
+              TableRow(children: [
+                const Text('hogwartsStudent'),
+                Center(child: Text(character.hogwartsStudent.toString())),
+              ]),
+              TableRow(children: [
+                const Text('wand'),
+                WandaInfo(wand: character.wand),
+              ]),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
