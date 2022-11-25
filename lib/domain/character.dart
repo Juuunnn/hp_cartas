40551e1dc6fa47b34a05311776aa33fd012d4328
Juuunnn <jpablo.uni@gmail.com>
2022-11-25 01:51:43 -0700

    converti las clases en el archivo en clases con datos

--- lib/domain/character.dart
+++ lib/domain/character.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:hp_cartas/domain/problem.dart';
@@ -14,6 +16,19 @@ class HPCharacter {
   final bool hogwartsStudent;
   final bool hogwartsStaff;
   final String? image;
+  HPCharacter({
+    required this.name,
+    required this.species,
+    required this.house,
+    required this.gender,
+    required this.dateOfBirth,
+    required this.ancestry,
+    required this.patronus,
+    this.wand,
+    required this.hogwartsStudent,
+    required this.hogwartsStaff,
+    this.image,
+  });
 
   HPCharacter._(
       {required this.name,
@@ -62,6 +77,110 @@ class HPCharacter {
       image: imageUrl,
     );
   }
+
+  HPCharacter copyWith({
+    String? name,
+    String? species,
+    String? house,
+    String? gender,
+    String? dateOfBirth,
+    String? ancestry,
+    String? patronus,
+    Barita? wand,
+    bool? hogwartsStudent,
+    bool? hogwartsStaff,
+    String? image,
+  }) {
+    return HPCharacter(
+      name: name ?? this.name,
+      species: species ?? this.species,
+      house: house ?? this.house,
+      gender: gender ?? this.gender,
+      dateOfBirth: dateOfBirth ?? this.dateOfBirth,
+      ancestry: ancestry ?? this.ancestry,
+      patronus: patronus ?? this.patronus,
+      wand: wand ?? this.wand,
+      hogwartsStudent: hogwartsStudent ?? this.hogwartsStudent,
+      hogwartsStaff: hogwartsStaff ?? this.hogwartsStaff,
+      image: image ?? this.image,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'species': species,
+      'house': house,
+      'gender': gender,
+      'dateOfBirth': dateOfBirth,
+      'ancestry': ancestry,
+      'patronus': patronus,
+      'wand': wand?.toMap(),
+      'hogwartsStudent': hogwartsStudent,
+      'hogwartsStaff': hogwartsStaff,
+      'image': image,
+    };
+  }
+
+  factory HPCharacter.fromMap(Map<String, dynamic> map) {
+    return HPCharacter(
+      name: map['name'] as String,
+      species: map['species'] as String,
+      house: map['house'] as String,
+      gender: map['gender'] as String,
+      dateOfBirth: map['dateOfBirth'] as String,
+      ancestry: map['ancestry'] as String,
+      patronus: map['patronus'] as String,
+      wand: map['wand'] != null
+          ? Barita.fromMap(map['wand'] as Map<String, dynamic>)
+          : null,
+      hogwartsStudent: map['hogwartsStudent'] as bool,
+      hogwartsStaff: map['hogwartsStaff'] as bool,
+      image: map['image'] != null ? map['image'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory HPCharacter.fromJson(String source) =>
+      HPCharacter.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'HPCharacter(name: $name, species: $species, house: $house, gender: $gender, dateOfBirth: $dateOfBirth, ancestry: $ancestry, patronus: $patronus, wand: $wand, hogwartsStudent: $hogwartsStudent, hogwartsStaff: $hogwartsStaff, image: $image)';
+  }
+
+  @override
+  bool operator ==(covariant HPCharacter other) {
+    if (identical(this, other)) return true;
+
+    return other.name == name &&
+        other.species == species &&
+        other.house == house &&
+        other.gender == gender &&
+        other.dateOfBirth == dateOfBirth &&
+        other.ancestry == ancestry &&
+        other.patronus == patronus &&
+        other.wand == wand &&
+        other.hogwartsStudent == hogwartsStudent &&
+        other.hogwartsStaff == hogwartsStaff &&
+        other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return name.hashCode ^
+        species.hashCode ^
+        house.hashCode ^
+        gender.hashCode ^
+        dateOfBirth.hashCode ^
+        ancestry.hashCode ^
+        patronus.hashCode ^
+        wand.hashCode ^
+        hogwartsStudent.hashCode ^
+        hogwartsStaff.hashCode ^
+        image.hashCode;
+  }
 }
 
 class Barita {
@@ -69,5 +188,55 @@ class Barita {
   final String core;
   final int length;
 
-  Barita({required this.wood, required this.core, required this.length});
+  Barita({
+    required this.wood,
+    required this.core,
+    required this.length,
+  });
+
+  Barita copyWith({
+    String? wood,
+    String? core,
+    int? length,
+  }) {
+    return Barita(
+      wood: wood ?? this.wood,
+      core: core ?? this.core,
+      length: length ?? this.length,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'wood': wood,
+      'core': core,
+      'length': length,
+    };
+  }
+
+  factory Barita.fromMap(Map<String, dynamic> map) {
+    return Barita(
+      wood: map['wood'] as String,
+      core: map['core'] as String,
+      length: map['length'] as int,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Barita.fromJson(String source) =>
+      Barita.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'Barita(wood: $wood, core: $core, length: $length)';
+
+  @override
+  bool operator ==(covariant Barita other) {
+    if (identical(this, other)) return true;
+
+    return other.wood == wood && other.core == core && other.length == length;
+  }
+
+  @override
+  int get hashCode => wood.hashCode ^ core.hashCode ^ length.hashCode;
 }
