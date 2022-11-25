661e4bdff25f4981626c03eb058c5fc732bdb7d0
Juuunnn <jpablo.uni@gmail.com>
2022-11-25 11:12:01 -0700

    primera version de la vista de la tarjeta de personaje

--- lib/feature/characterCard/view/character_card_view.dart
+++ lib/feature/characterCard/view/character_card_view.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:fpdart/fpdart.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hp_cartas/domain/character.dart';
 
 class CharacterCardView extends StatelessWidget {
@@ -18,47 +18,109 @@ class CharacterCardView extends StatelessWidget {
           'hogwartsStudent',
           'image'
         ].contains(key));
-    return SizedBox(
-      width: 300,
-      child: Column(
-        children: [
-          Text(character.name),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CachedNetworkImage(
-              imageUrl: character.image ??
-                  'https://via.placeholder.com/728x90.jpeg?text=No+Image+data',
-              placeholder: (context, url) => const CircularProgressIndicator(),
-              errorWidget: (context, url, error) => const Icon(Icons.error),
-            ),
-          ),
-          Table(
-            // defaultColumnWidth: const FlexColumnWidth(100),
-            border: TableBorder.symmetric(
-              outside: const BorderSide(color: Colors.black),
-              inside: const BorderSide(color: Colors.black),
-            ),
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 231, 203, 138),
+        border: Border.all(color: Colors.grey, width: 2),
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(7.0),
+        child: SizedBox(
+          width: 300,
+          child: Column(
             children: [
-              ...characterMap.entries.map((e) {
-                return TableRow(children: [
-                  Text(e.key),
-                  Center(child: Text(e.value.toString()))
-                ]);
-              }).toList(),
-              TableRow(children: [
-                const Text('hogwartsStaff'),
-                Center(child: Text(character.hogwartsStaff.toString()))
-              ]),
-              TableRow(children: [
-                const Text('hogwartsStudent'),
-                Center(child: Text(character.hogwartsStudent.toString())),
-              ]),
-              TableRow(children: [
-                const Text('wand'),
-                WandaInfo(wand: character.wand),
-              ]),
+              Container(
+                width: double.infinity,
+                color: Colors.brown,
+                child: Center(
+                  child: Text(
+                    character.name,
+                    style: GoogleFonts.aladin(fontSize: 30),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CachedNetworkImage(
+                  imageUrl: character.image ??
+                      'https://via.placeholder.com/728x90.jpeg?text=No+Image+data',
+                  placeholder: (context, url) =>
+                      const CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Table(
+                  // defaultColumnWidth: const FlexColumnWidth(100),
+                  border: TableBorder.symmetric(
+                    outside: const BorderSide(color: Colors.black),
+                    inside: const BorderSide(color: Colors.black),
+                  ),
+                  children: [
+                    ...characterMap.entries.map((e) {
+                      return TableRow(children: [
+                        CampoTabla(e.key),
+                        RegistroTabla(e.value.toString()),
+                      ]);
+                    }).toList(),
+                    TableRow(children: [
+                      const CampoTabla('hogwartsStaff'),
+                      RegistroTabla(character.hogwartsStaff.toString()),
+                    ]),
+                    TableRow(children: [
+                      const CampoTabla('hogwartsStudent'),
+                      RegistroTabla(character.hogwartsStudent.toString()),
+                    ]),
+                    TableRow(children: [
+                      const CampoTabla('wand'),
+                      WandaInfo(wand: character.wand),
+                    ]),
+                  ],
+                ),
+              ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class RegistroTabla extends StatelessWidget {
+  const RegistroTabla(
+    this.nombreCampo, {
+    Key? key,
+  }) : super(key: key);
+  final String nombreCampo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Text(nombreCampo),
+      ),
+    );
+  }
+}
+
+class CampoTabla extends StatelessWidget {
+  const CampoTabla(
+    this.nombreCampo, {
+    Key? key,
+  }) : super(key: key);
+  final String nombreCampo;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(nombreCampo),
         ],
       ),
     );
