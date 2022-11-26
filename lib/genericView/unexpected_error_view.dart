7068e3d13d41d9a38a44562d0dc821d699daa673
Juuunnn <jpablo.uni@gmail.com>
2022-11-26 13:46:09 -0700

    agregue detallado a la vista de problemas para errores insesperados

--- lib/genericView/unexpected_error_view.dart
+++ lib/genericView/unexpected_error_view.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/widgets.dart';
+import 'package:hp_cartas/domain/problem.dart';
 
-class UnexpectedError extends StatelessWidget {
-  const UnexpectedError({super.key, required this.errorMessage});
-  final String errorMessage;
+class UnexpectedErrorView extends StatelessWidget {
+  const UnexpectedErrorView({super.key, required this.problem});
+  final Problem problem;
   @override
   Widget build(BuildContext context) {
+    // print(object)
+    if (problem is UnknownProblem) {
+      return Center(
+        child: Column(
+          children: [
+            const Text('se genero un error inesperado en la aplicacion:'),
+            Text(
+                'Estado actual ${problem.toString()} ${(problem as UnknownProblem).problema}'),
+            // TODO: aqui deve haber alguna manera para reiniciar la aplicacion
+          ],
+        ),
+      );
+    }
     return Center(
       child: Column(
         children: [
           const Text('se genero un error inesperado en la aplicacion:'),
-          Text('Estado actual $errorMessage'),
+          Text('Estado actual ${problem.toString()}'),
           // TODO: aqui deve haber alguna manera para reiniciar la aplicacion
         ],
       ),
