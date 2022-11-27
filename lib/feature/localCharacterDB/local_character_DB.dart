8c9eee232bf42c2ed3564948e12b129c5961a5ff
Juuunnn <jpablo.uni@gmail.com>
2022-11-26 18:13:41 -0700

    agregue en un comentario lo que se espera implementar

--- lib/feature/localCharacterDB/local_character_DB.dart
+++ lib/feature/localCharacterDB/local_character_DB.dart
@@ -1,1 +1,4 @@
 //TODO: adminisrar personajes almacenados localmente y los obtenidos de la api
+// states: loading, new character unlocked, getting character, getting character list from api, bad comunication, bad response format
+// events: got character, got character list
+// screens: character obtained view,
