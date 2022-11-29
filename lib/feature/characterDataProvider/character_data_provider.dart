c277ac1774c86b6900a75f649c481039d84e0cd2
Juuunnn <jpablo.uni@gmail.com>
2022-11-29 10:18:13 -0700

    agregue vistas de error relacionadas con cargar datos de la api

--- lib/feature/characterDataProvider/character_data_provider.dart
+++ lib/feature/characterDataProvider/character_data_provider.dart
@@ -1,6 +1,9 @@
 // ignore: todo
 //TODO: adminisrar personajes almacenados localmente y los obtenidos de la api
-// states: loading, new character unlocked, getting character, getting character list from api, bad comunication, bad response format
+// states: loading, new character unlocked, getting character, getting character list from api,
+//
+//      finished:  bad comunication, bad response format
+
 // events: got character, got character list
 // screens: character obtained view,
 
