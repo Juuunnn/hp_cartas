d60617531f6efb63c58173a518bc20ff4bd3effa
Juuunnn <jpablo.uni@gmail.com>
2022-11-29 18:52:47 -0700

    mejor interaccion al obtener nuevo personaje

--- lib/feature/characterDataProvider/view/new_character_obtained_view.dart
+++ lib/feature/characterDataProvider/view/new_character_obtained_view.dart
@@ -15,42 +15,59 @@ class NewCharacterObtainedView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      behavior: HitTestBehavior.opaque,
       onTap: onTap,
       child: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 231, 203, 138),
-            border: Border.all(color: Colors.grey, width: 2),
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          child: SizedBox(
-            width: 300,
-            child: Column(
-              children: [
-                Container(
-                  width: double.infinity,
-                  color: Colors.brown,
-                  child: Center(
-                    child: Text(
-                      character.name,
-                      style: GoogleFonts.aladin(fontSize: 30),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CachedNetworkImage(
-                    imageUrl: character.image ??
-                        'https://via.placeholder.com/728x90.jpeg?text=No+Image+data',
-                    placeholder: (context, url) =>
-                        const CircularProgressIndicator(),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.error),
-                  ),
-                ),
-              ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'obtuviste un nuevo personaje!!',
+              style: GoogleFonts.aladin(fontSize: 50),
             ),
-          ),
+            Container(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 231, 203, 138),
+                border: Border.all(color: Colors.grey, width: 2),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(7.0),
+                child: SizedBox(
+                  width: 300,
+                  child: Column(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        color: Colors.brown,
+                        child: Center(
+                          child: Text(
+                            character.name,
+                            style: GoogleFonts.aladin(fontSize: 30),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CachedNetworkImage(
+                          imageUrl: character.image ??
+                              'https://via.placeholder.com/728x90.jpeg?text=No+Image+data',
+                          placeholder: (context, url) =>
+                              const CircularProgressIndicator(),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              'presiona la pantalla para continuar',
+              style: GoogleFonts.aladin(fontSize: 20),
+            ),
+          ],
         ),
       ),
     );
